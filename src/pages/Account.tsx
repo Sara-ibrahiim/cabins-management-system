ccfd3dcd403f11bffa22247ca990131d5be5a849
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-06-20 22:27:43 +0300

    tsx

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -10,6 +10,10 @@ function Account() {
         <Heading as="h3">Update user data</Heading>
         <p>Update user data form</p>
       </Row>
+ <Row>
+        <Heading as="h3">Update user data</Heading>
+        <p>Update user data form</p>
+      </Row>
 
       <Row>
         <Heading as="h3">Update password</Heading>
